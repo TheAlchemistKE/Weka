406511d57852f070483821698393eb22421e6148
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2020-10-21 00:24:15 +0300

    Fix rubocop linter issues.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: %i[show edit update destroy]
 
   # GET /tasks
   # GET /tasks.json
@@ -9,8 +9,7 @@ class TasksController < ApplicationController
 
   # GET /tasks/1
   # GET /tasks/1.json
-  def show
-  end
+  def show; end
 
   # GET /tasks/new
   def new
@@ -18,8 +17,7 @@ class TasksController < ApplicationController
   end
 
   # GET /tasks/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /tasks
   # POST /tasks.json
@@ -62,13 +60,14 @@ class TasksController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_task
-      @task = Task.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def task_params
-      params.require(:task).permit(:user_id, :name, :time_spent)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def task_params
+    params.require(:task).permit(:user_id, :name, :time_spent)
+  end
 end
--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -5,17 +5,17 @@ class TasksControllerTest < ActionDispatch::IntegrationTest
     @task = tasks(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get tasks_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get new_task_url
     assert_response :success
   end
 
-  test "should create task" do
+  test 'should create task' do
     assert_difference('Task.count') do
       post tasks_url, params: { task: { name: @task.name, time_spent: @task.time_spent, user_id: @task.user_id } }
     end
@@ -23,22 +23,22 @@ class TasksControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to task_url(Task.last)
   end
 
-  test "should show task" do
+  test 'should show task' do
     get task_url(@task)
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get edit_task_url(@task)
     assert_response :success
   end
 
-  test "should update task" do
+  test 'should update task' do
     patch task_url(@task), params: { task: { name: @task.name, time_spent: @task.time_spent, user_id: @task.user_id } }
     assert_redirected_to task_url(@task)
   end
 
-  test "should destroy task" do
+  test 'should destroy task' do
     assert_difference('Task.count', -1) do
       delete task_url(@task)
     end

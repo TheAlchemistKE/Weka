b47166e1668f587996337a94df25da3eaf20c942
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2020-10-29 21:25:34 +0300

    Create a mailer.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :tasks
   has_many :groups
+
+  after_action: send_welcome_email
+  def send_welcome_email
+    WelcomeMailer.send_welcome_email(self).deliver
+    redirect_to profile_index_path, alert: "Thank you for signing up."
+  end
 end

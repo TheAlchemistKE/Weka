8b49c6a7a01d1c7d4a041b0d33b40255bc660a96
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2020-10-30 22:26:11 +0300

    Implement TSE Review.

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ApplicationRecord
   belongs_to :user
   has_many :groups
+  belongs_to :group
   has_one_attached :icon
   validates :name, presence: true, length: { minimum: 3 }
   validates :amount, presence: true

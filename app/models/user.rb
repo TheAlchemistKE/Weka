3e29072190883406d0f1e44c6bb277b5f2aa7c20
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2020-10-30 09:08:33 +0300

    Remove Unnecessary files and comments.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,7 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :tasks
   has_many :groups
 
-  after_create :send_welcome_email
-  def send_welcome_email
-    WelcomeMailer.send_welcome_email(self).deliver
-  end
 end

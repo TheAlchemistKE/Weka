94a1b3483aae7c2110a6c54b4c1d6bfa48b02a97
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2020-10-30 09:25:04 +0300

    Fix broken features.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :tasks
   has_many :groups
-
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 50 }
+  validates :password, presence: true, length: { maximum: 20 }
 end

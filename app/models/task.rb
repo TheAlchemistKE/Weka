5954fe13c992de2b80da10663f1045772a93cd18
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2020-10-30 21:54:38 +0300

    Implement TSE Review.

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,9 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :user
   has_many :groups
-  belongs_to :group, optional: true
-  scope :tasks_with_group, -> { where.not(group_id: nil).includes(:group) }
-  scope :tasks_with_no_group, -> { where(group_id: nil) }
   has_one_attached :icon
   validates :name, presence: true, length: { minimum: 3 }
   validates :amount, presence: true

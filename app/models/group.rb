270aa8b66ab29e48368650e7f87fe2e77ea457cb
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2020-10-30 09:39:22 +0300

    Restrict routes to only controller methods that will be used.

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,7 @@
 class Group < ApplicationRecord
   belongs_to :user
   has_many :tasks
+  belongs_to :task
   has_one_attached :icon
   validates :name, presence: true, length: { minimum: 3 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  resources :groups
-  resources :tasks
+  resources :groups, only: [:create, :index, :show, :destroy, :new]
+  resources :tasks, only: [:create, :index, :show, :destroy, :new]
   resources :groups do
     resources :tasks
   end
-  resources :profile
-  resources :external_transactions
+  resources :profile, only: [:index]
+  resources :external_transactions, only: [:index]
   devise_for :users
   get '/user' => "profile#index", :as => :user_root
 
